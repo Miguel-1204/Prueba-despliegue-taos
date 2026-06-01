4161fb41f7c70fbf91366f1b56f05ead403079ae
Fernando <feranpepo010805@gmail.com>
2026-06-01 10:27:40 -0500

    Primer commit

---
+++ earring-store-api/src/main/java/co/com/TAOS/earring_store_api/EarringStoreApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.TAOS.earring_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarringStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarringStoreApiApplication.class, args);
+	}
+
+}
---
+++ earring-store-api/src/test/java/co/com/TAOS/earring_store_api/EarringStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.TAOS.earring_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarringStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
